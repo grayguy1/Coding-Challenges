e3dff471eda3a82c9b98740bab5615a2d8798a4b
Ahsan Wahab <41246568+grayguy1@users.noreply.github.com>
2021-09-22 21:48:03 -0400

    Time: 10 ms (70.96%), Space: 41.5 MB (73.04%) - LeetHub

--- top-k-frequent-elements/top-k-frequent-elements.java
+++ top-k-frequent-elements/top-k-frequent-elements.java
@@ -5,10 +5,18 @@ class Solution {
             countMap.put(nums[i], countMap.getOrDefault(nums[i], 0) + 1);
         }
         ArrayList<Integer> numSet = new ArrayList<>(countMap.keySet());
-        Collections.sort(numSet, (a, b) -> countMap.get(b) - countMap.get(a));
+        PriorityQueue<Integer> pq = new PriorityQueue<>(
+            (a, b) -> countMap.get(a) - countMap.get(b)
+        );
+        for (int i = 0; i < numSet.size(); i++) {
+            pq.add(numSet.get(i));
+            if (pq.size() > k) {
+                pq.poll();
+            }
+        }
         int[] result = new int[k];
         for (int i = 0; i < k; i++) {
-            result[i] = numSet.get(i);
+            result[i] = pq.poll();
         }
         return result;
     }
