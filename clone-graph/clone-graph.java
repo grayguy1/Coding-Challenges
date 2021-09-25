e054defc9c2023c7a3ce66d6010ca7dd1d2c9563
Ahsan Wahab <41246568+grayguy1@users.noreply.github.com>
2021-09-24 21:18:43 -0400

    Time: 25 ms (90.84%), Space: 39.2 MB (60.07%) - LeetHub

---
+++ clone-graph/clone-graph.java
@@ -0,0 +1,40 @@
+/*
+// Definition for a Node.
+class Node {
+    public int val;
+    public List<Node> neighbors;
+    public Node() {
+        val = 0;
+        neighbors = new ArrayList<Node>();
+    }
+    public Node(int _val) {
+        val = _val;
+        neighbors = new ArrayList<Node>();
+    }
+    public Node(int _val, ArrayList<Node> _neighbors) {
+        val = _val;
+        neighbors = _neighbors;
+    }
+}
+*/
+
+class Solution {
+    public Node cloneGraph(Node node) {
+        HashMap<Node, Node> visited = new HashMap<>();
+        return dfs(node, visited);
+    }
+    public Node dfs(Node node, HashMap<Node, Node> visited) {
+        if (node == null) {
+            return null;
+        }
+        if (visited.containsKey(node)) {
+            return visited.get(node);
+        }
+        Node clone = new Node(node.val, new ArrayList<Node>());
+        visited.put(node, clone);
+        for (Node neighbor : node.neighbors) {
+            clone.neighbors.add(dfs(neighbor, visited));
+        }
+        return clone;
+    }
+}
