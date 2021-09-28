5683b316bed7314cbc4f3d3fafd3fc2c09d155d5
Ahsan Wahab <41246568+grayguy1@users.noreply.github.com>
2021-09-28 18:16:57 -0400

    Time: 3 ms (79.50%), Space: 39.4 MB (62.11%) - LeetHub

--- valid-anagram/valid-anagram.java
+++ valid-anagram/valid-anagram.java
@@ -1,14 +1,15 @@
 class Solution {
     public boolean isAnagram(String s, String t) {
-        HashMap<Character, Integer> map = new HashMap<>();
+        if (s.length() != t.length()) {
+            return false;
+        }
+        int[] count = new int[26];
         for (int i = 0; i < s.length(); i++) {
-            map.put(s.charAt(i), map.getOrDefault(s.charAt(i), 0) + 1);
+            count[s.charAt(i) - 'a']++;
+            count[t.charAt(i) - 'a']--;
         }
-        for (int i = 0; i < t.length(); i++) {
-            map.put(t.charAt(i), map.getOrDefault(t.charAt(i), 0) - 1);
-        }
-        for (int i : map.values()) {
-            if (i != 0) {
+        for (int i = 0; i < 26; i++) {
+            if (count[i] != 0) {
                 return false;
             }
         }
