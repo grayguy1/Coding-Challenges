fb33d1e66156bbfabf613222600be573c1bf9e1f
Ahsan Wahab <41246568+grayguy1@users.noreply.github.com>
2021-10-09 14:48:07 -0400

    Time: 15 ms (78.15%), Space: 42.8 MB (57.88%) - LeetHub

---
+++ binary-search-tree-iterator/binary-search-tree-iterator.java
@@ -0,0 +1,52 @@
+/**
+ * Definition for a binary tree node.
+ * public class TreeNode {
+ *     int val;
+ *     TreeNode left;
+ *     TreeNode right;
+ *     TreeNode() {}
+ *     TreeNode(int val) { this.val = val; }
+ *     TreeNode(int val, TreeNode left, TreeNode right) {
+ *         this.val = val;
+ *         this.left = left;
+ *         this.right = right;
+ *     }
+ * }
+ */
+class BSTIterator {
+
+    Stack<TreeNode> stack;
+    public BSTIterator(TreeNode root) {
+        stack = new Stack<>();
+        while (root != null) {
+            stack.push(root);
+            root = root.left;
+        }
+    }
+    
+    public int next() {
+        TreeNode curr = stack.pop();
+        if (curr.right != null) {
+            TreeNode temp = curr.right;
+            while (temp != null) {
+                stack.push(temp);
+                temp = temp.left;
+            }
+        }
+        return curr.val;
+    }
+    
+    public boolean hasNext() {
+        if (!stack.isEmpty()) {
+            return true;
+        }
+        return false;
+    }
+}
+
+/**
+ * Your BSTIterator object will be instantiated and called as such:
+ * BSTIterator obj = new BSTIterator(root);
+ * int param_1 = obj.next();
+ * boolean param_2 = obj.hasNext();
+ */
